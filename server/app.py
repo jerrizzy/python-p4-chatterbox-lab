9767a6fc8dc965922dfc95eb5d98d1533d113330
Jerry Janvier <jerrythefilmmaker@gmail.com>
2024-04-05 13:22:03 -0400

    lab completed

--- server/app.py
+++ server/app.py
@@ -14,13 +14,44 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        all_messages = Message.query.all()
+        return [msg.to_dict() for msg in all_messages], 200
+    
+    elif request.method == 'POST':
+        json_data = request.get_json()
+        new_msg = Message(
+            body=json_data.get('body'),
+            username=json_data.get('username')
+        )
+        db.session.add(new_msg)
+        db.session.commit()
 
-@app.route('/messages/<int:id>')
+        return new_msg.to_dict(), 201
+
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    msg = Message.query.filter(Message.id == id).first()
+    if msg is None:
+        return {'error': 'message not found'}, 404
+    
+    if request.method == 'PATCH':
+        
+        json_data = request.get_json()
+        if 'body' in json_data:
+            msg.body = json_data.get('body')
+        db.session.add(msg)
+        db.session.commit()
+
+        return msg.to_dict(), 200
+    
+    elif request.method == 'DELETE':
+        db.session.delete(msg)
+        db.session.commit()
+        return {}, 204
+
 
 if __name__ == '__main__':
     app.run(port=5555)
